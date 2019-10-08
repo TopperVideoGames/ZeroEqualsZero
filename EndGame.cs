c66cf0f3d2aebc1f7ef3573228e28163c828210a
TopperVideoGames <56213762+TopperVideoGames@users.noreply.github.com>
2019-10-08 16:05:47 -0400

    EndGameScript
    Made a script to close the game when ESCAPE is pressed, for the windows build of the game.

---
+++ EndGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CloseGame();
+    }
+
+    void CloseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
